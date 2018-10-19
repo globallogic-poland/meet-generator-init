bc22e61fcb80f1220b4ea470f0a685413537f4ad
Robert Gonciarz <gonciarz@gmail.com>
2018-10-19 23:58:01 +0200

    init

---
+++ src/main/java/meet/generator/init/GeneratorInitApplication.java
@@ -0,0 +1,12 @@
+package meet.generator.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorInitApplication.class, args);
+	}
+}

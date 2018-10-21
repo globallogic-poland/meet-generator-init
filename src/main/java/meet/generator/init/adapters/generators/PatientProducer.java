5ab858d6314557cee89f3ffc2dd4a588fa7aa99d
Robert Gonciarz <gonciarz@gmail.com>
2018-10-21 23:32:46 +0200

    generator

--- src/main/java/meet/generator/init/adapters/generators/PatientProducer.java
+++ src/main/java/meet/generator/init/adapters/generators/PatientProducer.java
@@ -1,26 +1,54 @@
 package meet.generator.init.adapters.generators;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import meet.generator.init.adapters.generators.gen.DetailsGenerator;
+import meet.generator.init.adapters.generators.gen.LocationGenerator;
+import meet.generator.init.adapters.generators.results.Details;
+import meet.generator.init.adapters.generators.results.Location;
 import meet.generator.init.dto.Patient;
 import meet.generator.init.ports.DataProvider;
 import meet.generator.init.ports.generators.EntityProducer;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 @AllArgsConstructor
 public class PatientProducer implements EntityProducer<Patient> {
 
     private final DataProvider dataProvider;
+    private final DetailsGenerator detailsGenerator;
+    private final LocationGenerator locationGenerator;
 
     @Override
     public Flux<Patient> generate(long count) {
 
-//        Map<String, Country> countries = dataProvider.getCountries().getCountries();
-        dataProvider.getFirstNames().getFemales();
-        dataProvider.getFirstNames().getMales();
-        dataProvider.getLastNames().getLastNames();
+        Flux<Details> detailsFlux = Flux.fromIterable(() -> detailsGenerator);
+        Flux<Location> locationFlux = Flux.fromIterable(() -> locationGenerator);
+        // TODO: add age and diseases
+        // TODO: merge 2 streams
 
-        return Flux.just();
+//        Flux.zip(detailsFlux, locationFlux, (details, location) -> {
+//            log.info("{}", details);
+//            return Patient.builder().build();
+//        })
+//                .take(count);
 
-        
+        Details details = detailsGenerator.next();
+        Location location = locationGenerator.next();
+
+        Patient patient = Patient.builder()
+                .firstName(details.getFirstName())
+                .lastName(details.getLastName())
+//                .age() // TODO: add
+                .sex(details.getSex())
+//                .chronicDiseases() // TODO: add
+                .country(location.getCountry())
+                .city(location.getCity())
+                .district(location.getDistrict())
+                .build();
+
+        return Flux.just(patient);
+
+
     }
 }
--- src/main/java/meet/generator/init/ports/generators/Generator.java
+++ src/main/java/meet/generator/init/ports/generators/Generator.java
@@ -1,11 +1,16 @@
 package meet.generator.init.ports.generators;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
-public interface Generator<T> {
+public interface Generator<T> extends Iterator<T> {
 
     T next();
+
+    default boolean hasNext() {
+        return true;
+    }
 
     default <U> U random(U[] values, Random random) {
         int idx = random.nextInt(values.length);
